11bc8a2283a84c38ae5805efd26d88f4446f2503
Schabon Jorne <jorne.schabon@gmail.com>
2019-10-18 08:43:17 +0200

    Added findRoomsWithLesserScoreThan function
    Added a function that returns all the rooms that have a lesser happinessScore than the parameter

--- src/Model/PDORoomModel.php
+++ src/Model/PDORoomModel.php
@@ -11,17 +11,49 @@ class PDORoomModel implements RoomModel {
 
     public function findRoomByName($name)
     {
+        $this->validateName($name);
         $pdo = $this->connection->getPDO();
         $statement = $pdo->prepare('SELECT * FROM rooms WHERE name=:name');
         $statement->bindParam(':name', $name, \PDO::PARAM_STR);
         $statement->execute();
         $statement->bindColumn(1, $id, \PDO::PARAM_INT);
         $statement->bindColumn(2, $name, \PDO::PARAM_STR);
-        $statement->bindColumn(3, $happinessscore, \PDO::PARAM_INT);
+        $statement->bindColumn(3, $happinessScore, \PDO::PARAM_INT);
         $room = null;
         if ($statement->fetch(\PDO::FETCH_BOUND)) {
-            $room = ['id' => $id, 'name' => $name, 'happinessScore' => $happinessscore];
+            $room = ['id' => $id, 'name' => $name, 'happinessScore' => $happinessScore];
         }
         return $room;
+    }
+
+    public function findRoomsWithLesserHappinessScore ($happinessScore)
+    {
+        $this->validateHappinessScore($happinessScore);
+        $pdo = $this->connection->getPDO();
+        $statement = $pdo->prepare('SELECT * FROM rooms WHERE happinessScore < :score');
+        $statement->bindParam(':score', $happinessScore, \PDO::PARAM_INT);
+        $statement->execute();
+        $statement->bindColumn(1, $id, \PDO::PARAM_INT);
+        $statement->bindColumn(2, $name, \PDO::PARAM_STR);
+        $statement->bindColumn(3, $happinessScore, \PDO::PARAM_INT);
+        $rooms = [];
+        $counter = 0;
+        while ($statement->fetch(\PDO::FETCH_BOUND)) {
+            $rooms[$counter] = ['id' => $id, 'name' => $name, 'happinessScore' => $happinessScore];
+            $counter++;
+        }
+        return $rooms;
+    }
+
+    private function validateName ($name) {
+        if (!is_string($name)) {
+            throw new \InvalidArgumentException("Name moet een string bevatten");
+        }
+    }
+
+    private function validateHappinessScore($happinessScore) {
+        if (!(is_string($happinessScore) && preg_match("/^[0-9]+$/", $happinessScore))) {
+            throw new \InvalidArgumentException("Happinessscore moet een int bevatten");
+        }
     }
 }
