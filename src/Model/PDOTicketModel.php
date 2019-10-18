e8518a859658b054cb3cd0cf8cb7fbbdf2e28253
Schabon Jorne <jorne.schabon@gmail.com>
2019-10-18 08:44:51 +0200

    Added validators and increment function
    Added 2 validator-functions for Id and AssetName.  Added numberOfVotes incfement function

--- src/Model/PDOTicketModel.php
+++ src/Model/PDOTicketModel.php
@@ -13,18 +13,58 @@ class PDOTicketModel implements TicketModel {
 
     public function findTicketsByAssetName($assetName)
     {
+        $this->validateAssetName($assetName);
         $pdo = $this->connection->getPDO();
-        $statement = $pdo->prepare('SELECT * FROM tickets WHERE assetId = (SELECT assetId FROM assets WHERE name=:name)');
+        $statement = $pdo->prepare('SELECT * FROM tickets WHERE assetId = (SELECT id FROM assets WHERE name =:name)');
         $statement->bindParam(':name', $assetName, \PDO::PARAM_STR);
         $statement->execute();
         $statement->bindColumn(1, $id, \PDO::PARAM_INT);
         $statement->bindColumn(2, $assetId, \PDO::PARAM_INT);
         $statement->bindColumn(3, $numberOfVotes, \PDO::PARAM_INT);
         $statement->bindColumn(4, $description, \PDO::PARAM_STR);
-        $tickets = null;
-        if ($statement->fetch(\PDO::FETCH_BOUND)) {
-            $tickets = ['id' => $id, 'assetId' => $assetId, 'numberOfVotes' => $numberOfVotes, 'description' => $description];
+        $tickets = [];
+        $counter = 0;
+        while ($statement->fetch(\PDO::FETCH_BOUND)) {
+            $tickets[$counter] = ['id' => $id, 'assetId' => $assetId, 'numberOfVotes' => $numberOfVotes, 'description' => $description];
+            $counter++;
         }
         return $tickets;
+    }
+
+    public function addNumberOfVotesByOne($id) {
+        $this->validateId($id);
+        $pdo = $this->connection->getPDO();
+        $statement = $pdo->prepare('UPDATE tickets SET numberOfVotes = numberOfVotes + 1 WHERE id =:id');
+        $statement->bindParam(':id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+    }
+
+    public function findTicketById($id) {
+        $this->validateId($id);
+        $pdo = $this->connection->getPDO();
+        $statement = $pdo->prepare('SELECT * FROM tickets WHERE id=:id');
+        $statement->bindParam(':id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+        $statement->bindColumn(1, $id, \PDO::PARAM_INT);
+        $statement->bindColumn(2, $assetId, \PDO::PARAM_INT);
+        $statement->bindColumn(3, $numberOfVotes, \PDO::PARAM_INT);
+        $statement->bindColumn(4, $description, \PDO::PARAM_STR);
+        $ticket = null;
+        if ($statement->fetch(\PDO::FETCH_BOUND)) {
+            $ticket = ['id' => $id, 'assetId' => $assetId, 'numberOfVotes' => $numberOfVotes, 'description' => $description];
+        }
+        return $ticket;
+    }
+
+    private function validateId($id) {
+        if (!(is_string($id) && preg_match("/^[0-9]+$/", $id) && (int)$id > 0)) {
+            throw new \InvalidArgumentException("Id moet een int > 0 bevatten");
+        }
+    }
+
+    private function validateAssetName($assetName) {
+        if (!is_string($assetName)) {
+            throw new \InvalidArgumentException("AssetName moet een string bevatten");
+        }
     }
 }
